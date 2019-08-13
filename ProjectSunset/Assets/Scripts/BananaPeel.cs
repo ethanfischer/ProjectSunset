0db79590b3eef3577c7318478b8ed09d34b8eaa0
EthanFischer <ethan.f.fischer@gmail.com>
2019-08-12 19:23:05 -0500

    Add Banana Peel script

---
+++ ProjectSunset/Assets/Scripts/BananaPeel.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class BananaPeel : MonoBehaviour
+{
+    public int MinimumSlipFactor = 10;
+    public int MaximumSlipFactor = 50;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.IsBall())
+        {
+            SendBallInRandomDirection(other);
+        }
+    }
+
+    private void SendBallInRandomDirection(Collider other)
+    {
+        var rigidBody = other.GetComponent<Rigidbody>();
+        var randomizedSlipFactor = GetRandomizedSlipFactor();
+        var leftOrRightFactor = GetLeftOrRightFactor();
+        rigidBody.AddForce(new Vector3(randomizedSlipFactor * leftOrRightFactor, 0, 0));
+    }
+
+    private int GetRandomizedSlipFactor()
+    {
+        return Random.Range(MinimumSlipFactor, MaximumSlipFactor);
+    }
+
+    private int GetLeftOrRightFactor()
+    {
+        //Returns 1 or -1
+        return Random.Range(0, 2) * 2 - 1;
+    }
+}
