ba12bd770998b76ddc04b16b57f5966448894f22
EthanFischer <ethan.f.fischer@gmail.com>
2019-08-14 21:34:51 -0500

    Set time limit to 30 seconds

--- ProjectSunset/Assets/Scripts/Sunset.cs
+++ ProjectSunset/Assets/Scripts/Sunset.cs
@@ -4,7 +4,8 @@ using UnityEngine.SceneManagement;
 public class Sunset : MonoBehaviour
 {
     [Tooltip("The amount of time in seconds it takes for the sun to set")]
-    public float TimeLimit = 180; //default to 3 mins
+    public float TimeLimit = 30;
+
     private float _elapsedTime = 0.0f;
     private bool _didTimeExpire = false;
 
