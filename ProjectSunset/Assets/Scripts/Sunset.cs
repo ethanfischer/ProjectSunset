cff34fb2294c6e8e913701e15caf7304505e09ba
EthanFischer <ethan.f.fischer@gmail.com>
2019-08-14 21:26:49 -0500

    Add SceneNames and Sunset script

---
+++ ProjectSunset/Assets/Scripts/Sunset.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sunset : MonoBehaviour
+{
+    private const float TIME_LIMIT = 3;
+    private float _elapsedTime = 0.0f;
+    private bool _didTimeExpire = false;
+
+    private void Update()
+    {
+        if (_didTimeExpire)
+        {
+            return;
+        }
+
+        UpdateTimer();
+    }
+
+    private void UpdateTimer()
+    {
+        _elapsedTime += Time.deltaTime;
+
+        if (_elapsedTime >= TIME_LIMIT)
+        {
+            Debug.Log("Timer expired");
+            _didTimeExpire = true;
+            SceneManager.LoadScene(SceneNames.GAME_OVER);
+        }
+    }
+}
