f8d46b52f6eb396a1ec552d95548286536771445
EthanFischer <ethan.f.fischer@gmail.com>
2019-08-17 14:56:34 -0500

    Rename to gameovertimer

--- ProjectSunset/Assets/Scripts/GameOverTimer.cs
+++ ProjectSunset/Assets/Scripts/GameOverTimer.cs
@@ -1,9 +1,9 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Sunset : MonoBehaviour
+public class GameOverTimer : MonoBehaviour
 {
-    [Tooltip("The amount of time in seconds it takes for the sun to set")]
+    [Tooltip("The amount of time in seconds a gameover is triggered")]
     public float TimeLimit = 30;
 
     private float _elapsedTime = 0.0f;
