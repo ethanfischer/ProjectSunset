595297c72cafa115d12a895603e29468feab4f68
aallen170 <aallen170@gmail.com>
2019-08-17 14:27:05 -0500

    update

--- ProjectSunset/Assets/Scripts/PlayerController.cs
+++ ProjectSunset/Assets/Scripts/PlayerController.cs
@@ -2,10 +2,18 @@
 
 public class PlayerController : MonoBehaviour
 {
-    public float LeftRightSpeed;
-    public float ForwardMaxSpeed;
+    public float LeftRightSpeed = 1.5f;
+    public float ForwardMaxSpeed = 0.7f;
+    [Range(0, 3)] public float AccelerationSpeed = 3f;
+
+    private bool InTar = false;
+    private float CurrentMaxSpeed;
+    private float CurrentSpeed;
+    private float CurrentVelocity;
 
     private Rigidbody _rigidBody;
+
+    private Vector3 lastPos = Vector3.zero;
 
     void Start()
     {
@@ -14,10 +22,53 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
+        ImplementMovementEffects();
+
+        DebugControls();
+
         var moveHorizontal = Input.GetAxis("Horizontal");
 
-        var movement = new Vector3(moveHorizontal * LeftRightSpeed, 0, ForwardMaxSpeed);
+        var movement = new Vector3(moveHorizontal * LeftRightSpeed, 0, CurrentMaxSpeed);
 
         _rigidBody.AddForce(movement);
+
+        DebugOutput();
+    }
+
+    void ImplementMovementEffects()
+    {
+        if (InTar) CurrentMaxSpeed = 0f;
+        else CurrentMaxSpeed = ForwardMaxSpeed;
+    }
+
+    void DebugControls()
+    {
+        if (Input.GetKey(KeyCode.Space)) CurrentMaxSpeed = AccelerationSpeed;
+
+        if (Input.GetKey(KeyCode.LeftShift)) CurrentMaxSpeed = -CurrentMaxSpeed;
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            CurrentMaxSpeed = 0f;
+            _rigidBody.velocity = Vector3.zero;
+        }
+    }
+
+    void DebugOutput()
+    {
+        CurrentSpeed = (transform.position - lastPos).magnitude;
+        lastPos = transform.position;
+
+        CurrentVelocity = _rigidBody.velocity.magnitude;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag.Equals("tar")) InTar = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals("tar")) InTar = false;
     }
 }
---
+++ ProjectSunset/Assets/Scripts/Tar_Patch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tar_Patch : MonoBehaviour
+{
+    MeshCollider col;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<MeshCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player")) print("player in tar pit");
+    }
+}
