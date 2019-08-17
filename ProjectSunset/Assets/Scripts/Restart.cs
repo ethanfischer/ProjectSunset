041da2581540408c08165ccf36a3a8e6b6b750f4
EthanFischer <ethan.f.fischer@gmail.com>
2019-08-17 15:40:25 -0500

    Extract methods

--- ProjectSunset/Assets/Scripts/Restart.cs
+++ ProjectSunset/Assets/Scripts/Restart.cs
@@ -8,15 +8,35 @@ public class Restart : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            SceneManager.UnloadSceneAsync(SceneNames.GAME_OVER);
-            SceneManager.UnloadSceneAsync(SceneNames.TEST_LEVEL);
-            SceneManager.LoadSceneAsync(SceneNames.TEST_LEVEL, LoadSceneMode.Additive);
-            Player.Instance.Unfreeze();
-            PlayerSpawn.Instance.Spawn();
-            var dayNight = FindObjectOfType<WeatherMakerDayNightCycleManagerScript>();
-            dayNight.TimeOfDay = DayNightSettings.InitialTimeOfDay;
-            var gameOverTimer = FindObjectOfType<GameOverTimer>();
-            gameOverTimer.Reset();
+            ResetScenes();
+            ResetPlayer();
+            ResetDayNightCycle();
+            ResetGameOverTimer();
         }
+    }
+
+    private static void ResetScenes()
+    {
+        SceneManager.UnloadSceneAsync(SceneNames.GAME_OVER);
+        SceneManager.UnloadSceneAsync(SceneNames.TEST_LEVEL);
+        SceneManager.LoadSceneAsync(SceneNames.TEST_LEVEL, LoadSceneMode.Additive);
+    }
+
+    private static void ResetPlayer()
+    {
+        Player.Instance.Unfreeze();
+        PlayerSpawn.Instance.Spawn();
+    }
+
+    private static void ResetDayNightCycle()
+    {
+        var dayNight = FindObjectOfType<WeatherMakerDayNightCycleManagerScript>();
+        dayNight.TimeOfDay = DayNightSettings.InitialTimeOfDay;
+    }
+
+    private static void ResetGameOverTimer()
+    {
+        var gameOverTimer = FindObjectOfType<GameOverTimer>();
+        gameOverTimer.Reset();
     }
 }
