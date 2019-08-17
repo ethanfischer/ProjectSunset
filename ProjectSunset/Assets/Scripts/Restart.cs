5b51a10a3c975b341858fc507713b505f177c6b2
Ethan Fischer <ethan.f.fischer@gmail.com>
2019-08-17 16:43:00 -0500

    Game over + restart working with day/night cycle (#7)

--- ProjectSunset/Assets/Scripts/Restart.cs
+++ ProjectSunset/Assets/Scripts/Restart.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using DigitalRuby.WeatherMaker;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Restart : MonoBehaviour
@@ -7,7 +8,35 @@ public class Restart : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            SceneManager.LoadScene(SceneNames.MAIN);
+            ResetScenes();
+            ResetPlayer();
+            ResetDayNightCycle();
+            ResetGameOverTimer();
         }
+    }
+
+    private static void ResetScenes()
+    {
+        SceneManager.UnloadSceneAsync(SceneNames.GAME_OVER);
+        SceneManager.UnloadSceneAsync(SceneNames.TEST_LEVEL);
+        SceneManager.LoadSceneAsync(SceneNames.TEST_LEVEL, LoadSceneMode.Additive);
+    }
+
+    private static void ResetPlayer()
+    {
+        Player.Instance.Unfreeze();
+        PlayerSpawn.Instance.Spawn();
+    }
+
+    private static void ResetDayNightCycle()
+    {
+        var dayNight = FindObjectOfType<WeatherMakerDayNightCycleManagerScript>();
+        dayNight.TimeOfDay = DayNightSettings.InitialTimeOfDay;
+    }
+
+    private static void ResetGameOverTimer()
+    {
+        var gameOverTimer = FindObjectOfType<GameOverTimer>();
+        gameOverTimer.Reset();
     }
 }
