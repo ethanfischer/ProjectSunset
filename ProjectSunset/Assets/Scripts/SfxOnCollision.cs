b61be07a1903ffff8438cd0ff38026e98aa96148
EthanFischer <ethan.f.fischer@gmail.com>
2019-08-24 14:25:38 -0500

    WIP

---
+++ ProjectSunset/Assets/Scripts/SfxOnCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SfxOnCollision : MonoBehaviour
+{
+    private TrashSfx trashSfx;
+    void Start()
+    {
+    }
+
+    void OnCollisionEnter()  
+    {
+        trashSfx.Play();
+    }
+}
---
+++ ProjectSunset/Assets/Scripts/TrashSfx.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashSfx : MonoBehaviour
+{
+    private static TrashSfx _instance;
+    public static TrashSfx Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<TrashSfx>();
+            }
+
+            return _instance;
+        }
+    }
+
+    public AudioSource AudioSource;
+
+    public void Play()
+    {
+        AudioSource.Play();
+    }
+}
