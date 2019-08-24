3ccb00f2f1308c2c635137d3bbda883d37cba78f
EthanFischer <ethan.f.fischer@gmail.com>
2019-08-24 00:08:23 -0500

    Additively load MusicAndSfx scene

--- ProjectSunset/Assets/Scripts/LevelLoader.cs
+++ ProjectSunset/Assets/Scripts/LevelLoader.cs
@@ -6,5 +6,6 @@ public class LevelLoader : MonoBehaviour
     void Start()
     {
         SceneManager.LoadScene(SceneNames.TEST_LEVEL, LoadSceneMode.Additive);
+        SceneManager.LoadScene(SceneNames.MUSIC_AND_SFX, LoadSceneMode.Additive);
     }
 }
--- ProjectSunset/Assets/Scripts/SceneNames.cs
+++ ProjectSunset/Assets/Scripts/SceneNames.cs
@@ -3,4 +3,5 @@
     public const string MAIN = "Main";
     public const string GAME_OVER = "GameOver";
     public const string TEST_LEVEL = "TestLevel";
+    public const string MUSIC_AND_SFX = "MusicAndSfx";
 }
