c5da0536d833acc22eae4b5f634fad12d322a158
jasconrnbennett <47066018+jasconrnbennett@users.noreply.github.com>
2019-08-24 15:48:06 -0500

    Latest Changes (#20)

---
+++ ProjectSunset/Assets/Jason/Scripts/EasterEgg.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasterEgg : MonoBehaviour
+{
+
+    public GameObject fireWorks;
+
+    private void Start()
+    {
+        fireWorks.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            fireWorks.SetActive(true);
+        }
+    }
+}
