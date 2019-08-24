53b8c36454f8dfd3ee9e844ba8c28b8c7b9fb171
Jason <jason.r.bennett82@gmail.com>
2019-08-24 16:37:18 -0500

    Branch 3

---
+++ ProjectSunset/Assets/Jason/Scripts/WinGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinGame : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("You win!");
+        }
+    }
+
+}
