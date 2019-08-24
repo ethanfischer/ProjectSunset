e336a29dd7b5195614aa82dfed35209060fc3253
Jason <jason.r.bennett82@gmail.com>
2019-08-23 21:40:31 -0500

    JasonTest

---
+++ ProjectSunset/Assets/Jason/Scripts/TrashCan.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashCan : MonoBehaviour
+{
+
+    Rigidbody trashCanRB;
+    public float force = 30;
+    float angle;
+    bool spillOnce;
+    public GameObject trashPile;
+    public GameObject lidGO;
+    public GameObject bananaGO;
+    Rigidbody lidRB;
+    public Collider triggerCol;
+
+    void Start()
+    {
+        trashCanRB = GetComponent<Rigidbody>();
+        lidRB = lidGO.GetComponent<Rigidbody>();
+        spillOnce = true;
+        angle = transform.rotation.eulerAngles.x;
+    }
+
+    private void LateUpdate()
+    {
+        angle = transform.rotation.eulerAngles.x;
+    }
+
+    private void Update()
+    {
+        if (angle > 350 && spillOnce)
+        {
+            spillOnce = false;
+            lidRB.AddForce(transform.forward * (force * 5));
+            for (int i = 0; i < 2; i++)
+            {
+                Instantiate(trashPile, new Vector3(transform.position.x + 2f, transform.position.y +i, transform.position.z), Quaternion.identity);
+            }
+            //Instantiate(trashPile, new Vector3(transform.position.x + 2f, transform.position.y, transform.position.z), Quaternion.identity);
+            for (int i = 0; i < 4; i++)
+            {
+                Instantiate(bananaGO, new Vector3(transform.position.x + 2 + i, transform.position.y + 0.2f, transform.position.z), Quaternion.identity);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            trashCanRB.AddForce(transform.right * force);
+            triggerCol.enabled = false;
+        }
+    }
+
+}
